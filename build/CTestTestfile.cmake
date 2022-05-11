ab99ab224054af9457f77430332e8f30cb70e088
21020029 <hoanghoangminhtu1708@gmail.com>
2022-05-11 10:27:31 +0700

    delete some file and add binary file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/flower/dev/code/project/cplusplus/minesweeper
-# Build directory: /home/flower/dev/code/project/cplusplus/minesweeper/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
